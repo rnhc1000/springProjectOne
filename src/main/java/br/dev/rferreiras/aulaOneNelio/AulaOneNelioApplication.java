3af6c5689c6a53e5dfa4aa9dc3f8347e236e8b4d
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-05-12 21:18:41 -0300

    Fisrt commit

---
+++ src/main/java/br/dev/rferreiras/aulaOneNelio/AulaOneNelioApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rferreiras.aulaOneNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaOneNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaOneNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rferreiras/aulaOneNelio/AulaOneNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rferreiras.aulaOneNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaOneNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
