9071ae2757efb87c0b2aa6c9e5f801c393cf2d33
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-05-13 20:22:48 -0300

    Research Games by Id

--- src/main/java/br/dev/rferreiras/aulaOneNelio/controllers/GameController.java
+++ src/main/java/br/dev/rferreiras/aulaOneNelio/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.rferreiras.aulaOneNelio.dto.GameDTO;
 import br.dev.rferreiras.aulaOneNelio.dto.GameMinDTO;
-import br.dev.rferreiras.aulaOneNelio.entities.Game;
 import br.dev.rferreiras.aulaOneNelio.services.GameService;
 
 @RestController
@@ -16,12 +17,17 @@ import br.dev.rferreiras.aulaOneNelio.services.GameService;
 public class GameController {
 	@Autowired
 	private GameService gameService;
+	
 	@GetMapping
-	public List<GameMinDTO> findAll() {
-		
+	public List<GameMinDTO> findAll() {	
 		List<GameMinDTO> result = gameService.findAll();
-		return result;
-		
+		return result;	
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;	
 	}
 
 }
--- src/main/java/br/dev/rferreiras/aulaOneNelio/services/GameService.java
+++ src/main/java/br/dev/rferreiras/aulaOneNelio/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.dev.rferreiras.aulaOneNelio.dto.GameDTO;
 import br.dev.rferreiras.aulaOneNelio.dto.GameMinDTO;
 import br.dev.rferreiras.aulaOneNelio.entities.Game;
 import br.dev.rferreiras.aulaOneNelio.repositories.GameRepository;
@@ -15,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	//ACID
+	@Transactional(readOnly=true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;	
+	}
+	
+	@Transactional(readOnly=true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto =result.stream().map(x -> new GameMinDTO(x)).toList();
